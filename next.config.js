2ca344396e98220143d65cc7b4ae62958bb75565
Óscar Santana <osantana41@gmail.com>
2022-06-25 16:10:59 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+};
+
+module.exports = {
+	nextConfig,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles/sass')],
+	}
+};
